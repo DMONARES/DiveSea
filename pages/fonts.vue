bf6761c1c90c807a48cfcf3a6f5bcfd662214047
David <dmonaresss@gmail.com>
2025-03-28 21:09:33 +0300

    Add fonts

---
+++ pages/fonts.vue
@@ -0,0 +1,59 @@
+<script setup>
+
+</script>
+
+<template>
+	<div class="container">
+		<h1 class="heading--h1">Fonts</h1>
+		<h2 class="heading">DMsans</h2>
+		<h3 class="font" style="font-family: 'DMsans', sans-serif; font-weight: 700;">DMsans - Bold</h3>
+
+		<h2 class="heading">Inter</h2>
+		<h3 class="font"  style="font-family: 'Inter', sans-serif; font-weight: 500;">Inter - Medium</h3>
+		<h3 class="font"  style="font-family: 'Inter', sans-serif; font-weight: 700;">Inter - Bold</h3>
+
+		<h2 class="heading">Manrope</h2>
+		<h3 class="font"  style="font-family: 'Manrope', sans-serif; font-weight: 600;">Manrope - SemiBold</h3>
+		<h3 class="font"  style="font-family: 'Manrope', sans-serif; font-weight: 800;">Manrope - ExtraBold</h3>
+
+		<h2 class="heading">Outfit</h2>
+		<h3 class="font"  style="font-family: 'Outfit', sans-serif; font-weight: 400;">Outfit - Regular</h3>
+		<h3 class="font"  style="font-family: 'Outfit', sans-serif; font-weight: 500;">Outfit - Medium</h3>
+		<h3 class="font"  style="font-family: 'Outfit', sans-serif; font-weight: 600;">Outfit - SemiBold</h3>
+
+		<h2 class="heading">Poppins</h2>
+		<h3 class="font"  style="font-family: 'Poppins', sans-serif; font-weight: 400;">Poppins - Regular</h3>
+		<h3 class="font"  style="font-family: 'Poppins', sans-serif; font-weight: 500;">Poppins - Medium</h3>
+		<h3 class="font"  style="font-family: 'Poppins', sans-serif; font-weight: 600;">Poppins - SemiBold</h3>
+		<h3 class="font"  style="font-family: 'Poppins', sans-serif; font-weight: 700;">Poppins - Bold</h3>
+
+		<h2 class="heading">PublicSans</h2>
+		<h3 class="font"  style="font-family: 'PublicSans', sans-serif; font-weight: 600;">PublicSans - SemiBold</h3>
+		<h3 class="font"  style="font-family: 'PublicSans', sans-serif; font-weight: 700;">PublicSans - Bold</h3>
+
+		<h2 class="heading">Roboto</h2>
+		<h3 class="font"  style="font-family: 'Roboto', sans-serif; font-weight: 400;">Roboto - Regular</h3>
+		<h3 class="font"  style="font-family: 'Roboto', sans-serif; font-weight: 700;">Roboto - Bold</h3>
+
+		<h2 class="heading">Urbanist</h2>
+		<h3 class="font"  style="font-family: 'Urbanist', sans-serif; font-weight: 700;">Urbanist - Bold</h3>
+	</div>
+</template>
+
+<style lang='scss' scoped>
+.container {
+	padding: 0 50px;
+}
+.heading--h1 {
+	font-size: 50px;
+	margin-bottom: 100px;
+}
+.font {
+	font-size: 24px;
+	margin-bottom: 20px;
+}
+.heading {
+	font-size: 40px;
+	margin-bottom: 40px;
+}
+</style>
