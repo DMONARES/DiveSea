0e92458814b2d355f56a2782d09d0790357c95a1
David <dmonaresss@gmail.com>
2025-04-16 12:23:01 +0300

    Add poster for User profile

---
+++ components/profile/banner.vue
@@ -0,0 +1,43 @@
+<script setup>
+import { useRecommendStore } from "~/stores/recommend";
+
+const recommendStore = useRecommendStore();
+
+const product = recommendStore.slides.find((item) => item.poster);
+</script>
+
+<template>
+	<div class="profile-banner">
+		<img :src="product.poster" alt="" />
+	</div>
+</template>
+
+<style lang="scss" scoped>
+.profile-banner {
+	margin: 100px auto 75px;
+	padding: 0 96px;
+	border-radius: 20px;
+	overflow: hidden;
+
+	img {
+		margin: 0 auto;
+		border-radius: 20px;
+		overflow: hidden;
+
+		@media (max-width: 850px) {
+			border-radius: 0;
+		}
+	}
+
+	@media (max-width: 1200px) {
+		margin: 70px auto 50px;
+		padding: 0 70px;
+	}
+
+	@media (max-width: 850px) {
+		margin: 60px auto 100px;
+		border-radius: 0;
+		padding: 0;
+	}
+}
+</style>
--- pages/profile/[id].vue
+++ pages/profile/[id].vue
@@ -9,14 +9,17 @@ const recommendStore = useRecommendStore();
 const product = recommendStore.slides.find(
 	(item) => String(item.nickname) === String(userUrl)
 );
+//Устанавливаем Title для страницы
+const title = route.params.nickname || route.path.split("/").pop();
+useHead(() => ({
+	title: 'Профиль пользователя — ' + title,
+}));
 </script>
 
 <template>
 	<main>
-{{ product }}
+		<ProfileBanner />
 	</main>
 </template>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
